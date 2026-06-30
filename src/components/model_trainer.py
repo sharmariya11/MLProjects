3aeb77cb84fc3db6e863c187ae0dfc046d3a7b8f
Riya Sharma <sharmariya7225@gmail.com>
2026-06-30 21:18:19 +0530

    Hyperparameter Training

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -8,6 +8,7 @@ from sklearn.ensemble import (
     GradientBoostingRegressor,
     RandomForestRegressor,
 )
+
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 from sklearn.neighbors import KNeighborsRegressor
@@ -48,12 +49,12 @@ class ModelTrainer:
             }
             params={
                 "Decision Tree": {
-                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    'criterion':['squared_error', 'absolute_error', 'poisson'],
                      'splitter':['best','random'],
                      'max_features':['sqrt','log2'],
                 },
                 "Random Forest":{
-                     'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                     'criterion':['squared_error', 'absolute_error', 'poisson'],
                  
                      'max_features':['sqrt','log2',None],
                     'n_estimators': [8,16,32,64,128,256]
@@ -62,8 +63,7 @@ class ModelTrainer:
                      'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
                     'learning_rate':[.1,.01,.05,.001],
                     'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
-                     'criterion':['squared_error', 'friedman_mse'],
-                     'max_features':['auto','sqrt','log2'],
+                     'max_features': ['sqrt', 'log2',None],
                     'n_estimators': [8,16,32,64,128,256]
                 },
                 "Linear Regression":{},
